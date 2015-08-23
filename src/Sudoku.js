ad02e6da6e82fe660a4ed23569f12164ead68e5b
David Piersol-Freedman <david_piersol-freedman@intuit.com>
2015-08-22 20:50:48 -0400

    initial commit

---
+++ src/Sudoku.js
@@ -0,0 +1,284 @@
+'use strict';
+
+/**
+* Object containing all functions able to be performed on a sudoku puzzle array. Note that
+* this follows the functional paradigm as opposed to OO, as that seems to make more sense.
+*/
+var Sudoku = {
+
+  generatePuzzleArray: function () {
+
+    var puzzleArray = this.getEmptyPuzzleArray(),
+      cellValue,
+      possibleNums;
+
+    // Walk through each cell and see what numbers it can contain based on its row, column and quadrant.
+    OUTER: for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        cellValue = puzzleArray[i][j];
+        if (!cellValue) {
+          possibleNums = this.getPossibleNumbers(puzzleArray, j, i);
+          if (possibleNums.length === 0) {
+            // We've reached a point where the cell cannot legally contain any number, so start over.
+            return this.generatePuzzleArray();
+            break OUTER;
+          } else {
+            // If only one possible number was found, use that. Else, get a random number from the array.
+            puzzleArray[i][j] = (possibleNums.length === 1) ? possibleNums[0] : possibleNums[this.generateRandomArrayIndex(possibleNums.length)];
+          }
+        }
+      }
+    }
+
+    return puzzleArray;
+
+  },
+
+  /*
+  * Strip #s from the puzzle until it's at its minimal solvable state.
+  */
+  stripPuzzle: function (puzzleArray) {
+
+    var strippedPuzzleArray = this.clonePuzzleArray(puzzleArray),
+      i,
+      j,
+      coords,
+      usedCoords = [], // cache the coords we've tried in this array
+      num,
+      clone,
+      solved;
+
+    while (usedCoords.length !== 81) {
+      i = this.generateRandomArrayIndex();
+      j = this.generateRandomArrayIndex();
+      coords = '' + i + j;
+      if (usedCoords.indexOf(coords) != -1) {
+        // If we've already tried these coords, continue.
+        continue;
+      }
+      // cache the coords
+      usedCoords.push(coords);
+      // Grab the number from this location, then set it to 0.
+      num = strippedPuzzleArray[i][j];
+      strippedPuzzleArray[i][j] = 0;
+      // Clone the puzzle and see if it's solvable.
+      clone = this.clonePuzzleArray(strippedPuzzleArray);
+      solved = this.solvePuzzle(clone);
+      if (!this.isFull(solved)) {
+        // If it's not solvable, reset the cell to its original number.
+        strippedPuzzleArray[i][j] = num;
+      }
+    }
+
+    return strippedPuzzleArray;
+
+  },
+
+  /*
+  * Attempt to solve the puzzle. Note that this modifies the passed-in puzzle array!
+  */
+  solvePuzzle: function (puzzleArray) {
+
+    var cellValue,
+      possibleNums,
+      populatedCells = 0;
+
+    // Find each unknown cell and see what numbers it can contain based on its row, column and quadrant.
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        cellValue = puzzleArray[i][j];
+        if (!cellValue) {
+          possibleNums = this.getPossibleNumbers(puzzleArray, j, i);
+          if (possibleNums.length === 1) {
+            // If only one number found, update the puzzle array
+            puzzleArray[i][j] = possibleNums[0];
+            populatedCells++;
+          }
+        }
+      }
+    }
+
+    // If we're done, return.
+    if (populatedCells === 0) {
+      return puzzleArray;
+    }
+
+    // Else, recurse.
+    return this.solvePuzzle(puzzleArray);
+
+  },
+
+  /*
+  * Try to validate the solution.
+  */
+  validateSolution: function (puzzleArray) {
+
+    var cellValue,
+      possibleNums;
+
+    // Walk through each cell and see what numbers it can contain based on its row, column and quadrant.
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        // Get the value of the cell
+        cellValue = puzzleArray[i][j];
+        possibleNums = this.getPossibleNumbers(puzzleArray, j, i);
+        if (possibleNums.length !== 1 || cellValue !== possibleNums[0]) {
+          // If we found more than one possible #, or if the cell value does not match what we found, the solution is incorrect.
+          return false;
+        }
+      }
+    }
+
+    return true;
+
+  },
+
+  generateRandomArrayIndex: function (arrayLength) {
+    arrayLength = arrayLength || 9;
+    return Math.floor((Math.random() * arrayLength));
+  },
+
+  /*
+  * See if two puzzleArrays are equal
+  */
+  equals: function (puzzleArray1, puzzleArray2) {
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (puzzleArray1[i][j] !== puzzleArray2[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  },
+
+  /*
+  * Get a new, empty 9x9 puzzle array
+  */
+  getEmptyPuzzleArray: function () {
+    var puzzleArray = new Array(9);
+    for (var i = 0; i < 9; i++) {
+      puzzleArray[i] = new Array(9);
+    }
+    return puzzleArray;
+  },
+
+  clonePuzzleArray: function (puzzleArray) {
+    var arr = [],
+      subArray = [];
+    for (var i = 0; i < 9; i++) {
+      subArray = [];
+      for (var j = 0; j < 9; j++) {
+        subArray.push(puzzleArray[i][j]);
+      }
+      arr.push(subArray);
+    }
+    return arr;
+  },
+
+  /*
+  * Check if all cells are populated
+  */
+  isFull: function (puzzleArray) {
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (!puzzleArray[i][j]) return false;
+      }
+    }
+    return true;
+  },
+
+  /*
+  * Check if all cells are empty
+  */
+  isEmpty: function (puzzleArray) {
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (puzzleArray[i][j]) return false;
+      }
+    }
+    return true;
+  },
+
+  /*
+  * Examines the puzzle array, and gets all possible numbers for a given cell
+  * @param puzzle array
+  * @param x coord
+  * @param y coord
+  */
+  getPossibleNumbers: function (puzzleArray, x, y) {
+
+    var numbersInRow = this.examineRow(puzzleArray, x, y),
+      numbersInColumn = this.examineColumn(puzzleArray, x, y),
+      numbersInQuadrant = this.examineQuadrant(puzzleArray, x, y),
+      existingNums = [].concat(numbersInRow, numbersInColumn, numbersInQuadrant),
+      possibleNums = [];
+
+    for (var num = 1; num <= 9; num++) {
+      if (existingNums.indexOf(num) === -1) {
+        possibleNums.push(num);
+      }
+    }
+
+    return possibleNums;
+
+  },
+
+  /*
+  * Return an array of all numbers in a row, except for the number at x
+  */
+  examineRow: function (puzzleArray, x, y) {
+    var arr = puzzleArray[y].filter(function(elem, index, array) {
+      return index !== x && elem > 0;
+    });
+    return arr;
+  },
+
+  /*
+  * Return an array of all numbers in a column, except for the number at y
+  */
+  examineColumn: function (puzzleArray, x, y) {
+    var arr = [],
+      num;
+    for (var i = 0; i < 9; i++) {
+      if (i !== y) {
+        num = puzzleArray[i][x];
+        if (num) arr.push(num);
+      }
+    }
+    return arr;
+  },
+
+  /*
+  * Return an array of all numbers in a quadrant, except for the number at x/y
+  */
+  examineQuadrant: function (puzzleArray, x, y) {
+    var arr = [],
+      num,
+      quadrantCoords = this.getQuadrantCoords(x, y);
+    for (var i = quadrantCoords[0]; i < quadrantCoords[0] + 3; i++) {
+      for (var j = quadrantCoords[1]; j < quadrantCoords[1] + 3; j++) {
+        if (x === j && y === i) continue;
+        num = puzzleArray[i][j];
+        if (num) arr.push(num);
+      }
+    }
+    return arr;
+  },
+
+  /*
+  * Given x/y coords, get the starting point for the quadrant that the cell lives in
+  */
+  getQuadrantCoords: function (x, y) {
+    if (x < 3 && y < 3) return [0,0];
+    if (x < 6 && y < 3) return [0,3];
+    if (x < 9 && y < 3) return [0,6];
+    if (x < 3 && y < 6) return [3,0];
+    if (x < 6 && y < 6) return [3,3];
+    if (x < 9 && y < 6) return [3,6];
+    if (x < 3 && y < 9) return [6,0];
+    if (x < 6 && y < 9) return [6,3];
+    if (x < 9 && y < 9) return [6,6];
+  }
+
+}
---
+++ src/view.js
@@ -0,0 +1,177 @@
+'use strict';
+
+/*
+* Listen for changes to the puzzle array, and update the sudoku board.
+*/
+/*
+document.addEventListener('puzzleArrayChangeEvent', function (e) {
+    var cell = document.getElementById('cell' + e.y + e.x);
+    cell.textContent = e.num;
+}, false);
+*/
+
+/*
+document.addEventListener('sudokuBoardChangeEvent', function (e) {
+    var arr = boardToArray();
+    var bool = equals(arr, puzzleArray);
+    document.getElementById('resetPuzzleButton').disabled = bool;
+}, false);
+*/
+
+/*
+* Publish an event when a cell value in the puzzle array changes.
+*/
+/*
+function publishPuzzleArrayChangeEvent (x, y, num) {
+    var event = new Event('puzzleArrayChangeEvent');
+    event.x = x;
+    event.y = y;
+    event.num = num;
+    document.dispatchEvent(event);
+}
+*/
+
+/*
+* Publish an event when a number is entered in a cell in the sudoku board.
+*/
+/*
+function publishSudokuBoardChangeEvent (e) {
+    var event = new Event('sudokuBoardChangeEvent'),
+        id = e.target.id; // ID is in the form: cellYX
+    event.y = id.charAt(4);
+    event.x = id.charAt(5);
+    event.num = parseInt(e.target.textContent, 10);
+    document.dispatchEvent(event);
+}
+*/
+
+/*
+* Called via setInterval, continually monitors the board state and adjusts the UI as necessary.
+*/
+function examineBoardState () {
+  var boardArray = boardToArray();
+  var arraysAreEqual = Sudoku.equals(boardArray, strippedPuzzleArray);
+  document.getElementById('resetPuzzleButton').disabled = arraysAreEqual;
+  var boardIsFull = Sudoku.isFull(boardArray);
+  document.getElementById('solvePuzzleButton').disabled = boardIsFull;
+  if (boardIsFull) {
+    var solutionIsCorrect = Sudoku.validateSolution(boardArray);
+    if (solutionIsCorrect) {
+      document.getElementById('msg').textContent = 'Puzzle solved!';
+    } else {
+      document.getElementById('msg').textContent = 'Sorry, your solution is incorrect.';
+    }
+  } else {
+    document.getElementById('msg').textContent = '';
+  }
+  //var boardIsEmpty = isEmpty(boardArray);
+  //document.getElementById('clearPuzzleButton').disabled = boardIsEmpty;
+}
+
+var strippedPuzzleArray;
+
+function generateNewPuzzle () {
+  var puzzleArray = Sudoku.generatePuzzleArray();
+  strippedPuzzleArray = Sudoku.stripPuzzle(puzzleArray);
+  populateBoard(strippedPuzzleArray);
+}
+
+function resetPuzzle () {
+  populateBoard(strippedPuzzleArray);
+}
+
+function solveIt () {
+  var puzzleArray = boardToArray(),
+      solvedPuzzleArray = Sudoku.solvePuzzle(puzzleArray);
+  if (Sudoku.validateSolution(solvedPuzzleArray)) {
+    populateBoard(solvedPuzzleArray);
+  } else {
+    alert('Cannot solve puzzle in its current state!');
+  }
+}
+
+/*
+* Build the table that represents the sudoku board
+*/
+function buildSudokuBoard () {
+  var table = document.createElement('table'),
+      tBody = document.createElement('tbody'),
+      tr,
+      td;
+  for (var i = 0; i < 9; i++) {
+    tr = document.createElement('tr');
+    for (var j = 0; j < 9; j++) {
+      td = document.createElement('td');
+      td.id = 'cell' + i + j;
+      td.onkeypress = checkUserInput;
+      // Publish an event when a number is input into a cell on the sudoku board
+      //td.oninput = publishSudokuBoardChangeEvent;
+      tr.appendChild(td);
+    }
+    tBody.appendChild(tr);
+  }
+  table.appendChild(tBody);
+  document.getElementById('sudokuBoard').appendChild(table);
+};
+
+/*
+* Populate the sudoku board with the values from the puzzle array
+*/
+function populateBoard (puzzleArray) {
+  var num, cell;
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      num = puzzleArray[i][j];
+      cell = document.getElementById('cell' + i + j);
+      if (num) {
+        cell.textContent = num;
+        cell.contentEditable = false;
+      } else {
+        cell.textContent = '';
+        cell.contentEditable = true;
+      }
+    }
+  }
+}
+
+/*
+* Clear the sudoku board
+*/
+function clearBoard () {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      document.getElementById('cell' + i + j).textContent = '';
+    }
+  }
+}
+
+/*
+* Check user input
+*/
+function checkUserInput (e) {
+  if (e.keyCode === 9) {
+    // Tab key - move to next cell.
+    e.target.nextSibling.focus();
+  } else if (e.target.textContent || e.keyCode < 49 || e.keyCode > 57) {
+    // Target is aleady populated, or the key is something other than 1-9
+    e.preventDefault();
+  }
+}
+
+/*
+* Build a 2-dimensional array from the values on the board
+*/
+function boardToArray () {
+  var arr = [],
+      subArray = [];
+  for (var i = 0; i < 9; i++) {
+    subArray = [];
+    for (var j = 0; j < 9; j++) {
+      var num = parseInt(document.getElementById('cell' + i + j).textContent, 10);
+      if (isNaN(num)) num = 0;
+        subArray.push(num);
+      }
+      arr.push(subArray);
+  }
+  return arr;
+}
